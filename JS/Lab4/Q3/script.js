490d5f50bb7681184180bc4d8daff9d9dccca65a
Andrew Emad <andrewemad@Andrews-MacBook-Pro.local>
2026-01-10 21:53:33 +0200

    js Lab_4 Q3

---
+++ JS/Lab4/Q3/script.js
@@ -0,0 +1,29 @@
+var para = document.getElementById("PAR");
+function ChangeFont(x){
+    para.style.fontFamily = x;
+
+}
+function ChangeAlign(y){
+    para.style.textAlign = y;
+}
+function ChangeHeight(height){
+    para.style.lineHeight = height;
+}
+function ChangeLSpace(space){
+    para.style.letterSpacing = space;
+}
+function ChangeIndent(indent){
+    para.style.textIndent = indent;
+}
+function ChangeTransform(transformType){
+    para.style.textTransform = transformType;
+}
+function ChangeDecorate(decoration){
+    para.style.textDecoration = decoration;
+}
+function ChangeBorder(borderType){
+    para.style.borderStyle = borderType;
+}
+function ChangeBorderColor(color){
+    para.style.borderColor = color;
+}
